fcf8a006a602582d9ab17068000d5139c5d2c66e
IvyZh <app_do@163.com>
2019-06-10 17:36:50 +0800

    Initial commit

---
+++ springboot_quick/src/main/java/com/ivyzh/springboot/controller/QuickController.java
@@ -0,0 +1,13 @@
+package com.ivyzh.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuickController {
+    @RequestMapping("/quick")
+    public String quick() {
+        return "quick...";
+    }
+}
